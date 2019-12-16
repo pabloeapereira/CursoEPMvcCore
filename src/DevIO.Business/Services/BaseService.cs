2bdcfd542842a8d6dfde7f6ade3f8ca5b8689f10
Pablo Emanuel <pablo.pereira@stand.com.br>
2019-12-16 09:03:37 -0300

    FluentValidation

---
+++ src/DevIO.Business/Services/BaseService.cs
@@ -0,0 +1,31 @@
+﻿using DevIO.Business.Models;
+using FluentValidation.Results;
+using FluentValidation;
+namespace DevIO.Business.Services
+{
+    public abstract class BaseService
+    {
+        protected void Notificar(string mensagem)
+        {
+
+        }
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach(var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected bool ExecutarValidacao<TV,TE>(TV validacao, TE entidade) where TV:AbstractValidator<TE> where TE:Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
+    }
+}
---
+++ src/DevIO.Business/Services/ProdutoService.cs
@@ -0,0 +1,30 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public class ProdutoService : BaseService, IFornecedorService
+    {
+        public async Task Adicionar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Atualizar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AtualizarEndereco(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
